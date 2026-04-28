5fb1f22e0dfc3785cde2db8146303ee152d3669b
IgnacioSerrano04 <149999444+IgnacioSerrano04@users.noreply.github.com>
2026-04-28 13:59:02 -0600

    Create CalculadoraTest.java

---
+++ src/test/java/CalculadoraTest.java
@@ -0,0 +1,23 @@
+x = 5
+y = 2
+public class Calculadora
+{
+public int suma (int x, int y)
+{
+int resultado;
+resultado = x + y;
+return resultado;
+}
+public int resta (int x, int y)
+{
+int resultado;
+resultado = x - y;
+return resultado;
+}
+public int multiplica (int x, int y)
+{
+int resultado;
+resultado = x + y;
+return resultado;
+}
+}
